192b7f26d29069c3cd583d15d9a17ddf0d435767
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2023-12-29 11:26:42 -0300

    feat: #4 cria menu de produtos

--- cosmo-bank/src/app/app.module.ts
+++ cosmo-bank/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
